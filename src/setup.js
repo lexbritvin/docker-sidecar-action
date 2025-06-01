2ff9b0f50c6ce6d21c6fb69794b1924de7b2f552
Aleksandr Britvin <lexbritvin@gmail.com>
2025-06-01 23:48:50 +0200

    WIP

--- src/setup.js
+++ src/setup.js
@@ -16,7 +16,8 @@ export async function setup() {
   // Read connection details
   const details = fs.readFileSync("sidecar-details.env", "utf8");
   const dockerHost = details.match(/DOCKER_HOST=(.*)/)[1];
-  const tlsVerify = details.match(/DOCKER_TLS_VERIFY=(.*)/)[1] === "1";
+  const tlsMatch = details.match(/DOCKER_TLS_VERIFY=(.*)/);
+  const tlsVerify = tlsMatch ? tlsMatch[1] === "1" : false;
 
   // Set Docker host
   core.exportVariable("DOCKER_HOST", dockerHost);
