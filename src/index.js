4a2227c04769f1f749488b9bfb426028e9ca6fc2
Aleksandr Britvin <lexbritvin@gmail.com>
2025-06-04 01:44:15 +0200

    WIP

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import * as core from "@actions/core";
-
-import { setup } from "./setup.js";
-
-try {
-  await setup()
-} catch (error) {
-  core.setFailed(`Action failed with error: ${error.message}`);
-}
